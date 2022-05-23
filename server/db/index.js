5ca4255e5685084211c12044d001f321aaa7ce17
CamDavi5 <camrdavis.biz@gmail.com>
2022-05-22 20:31:09 -0500

    code cleanup and marked keys

--- server/db/index.js
+++ server/db/index.js
@@ -4,8 +4,8 @@ import Fetch from './fetch';
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root', //replace this
-    password: 'rootPassword', //replace this
+    user: 'root', //replace this with MySQL username
+    password: 'rootPassword', //replace this with MySQL password
     database: 'fetchuser'
 });
 
